c863e63e4847f6747103eb6635e5575d67f1e65e
layne <layne@atguigu.com>
2021-12-06 16:43:26 +0800

    hot-fix2 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello git5");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2");
     }
 }
